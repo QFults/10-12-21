98043613462685b65d4f15d1cf6c413cf904872a
Quinton Fults <quinton.fults.work@gmail.com>
2021-10-12 11:10:10 -0700

    code

---
+++ schemas/typeDefs.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+  type Item {
+    _id: ID!
+    text: String!
+    isDone: Boolean!
+  }
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    items: [Item!]
+  }
+  type Query {
+    items: [Item!]
+    item(_id: ID!): Item!
+  }
+  type Mutation {
+    addItem(text: String!, isDone: Boolean!): Item!
+    markDone(_id: ID!, isDone: Boolean!): Item
+    deleteItem(_id: ID!): Item
+  }
+`
+
+module.exports = typeDefs
---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const { join } = require('path')
+const syncDB = require('./db')
+
+const { ApolloServer } = require('apollo-server-express')
+const { typeDefs, resolvers } = require('./schemas')
+
+const app = express()
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers  
+})
+
+server.applyMiddleware(app)
+
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
+})
+
+syncDB()
+  .then(() => app.listen(process.env.PORT || 3001))
+  .catch(err => console.log(err))
